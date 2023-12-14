612a8aad9d8de3ced1f48416b81b542196d76839
Sam Elliott <soe1@uakron.edu>
2023-12-13 22:17:40 -0500

    dunno why ESLint is acting up the way it is, I wonder if I can have someone else look at this for me?

---
+++ learning-tests/.eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
+    tsconfigRootDir: 'learning_unit_tests_and_static_analysis/learning-tests',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+  ],
+  rules: {
+  },
+};
