2ef73ba7e3675686e87532bc372c340b35e25af2
Sam Elliott <soe1@uakron.edu>
2023-12-30 12:44:09 -0500

    alright, now we have ESLINT working as intended

--- learning-tests/.eslintrc.cjs
+++ learning-tests/.eslintrc.cjs
@@ -1,38 +1,41 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb',
-    'airbnb-typescript',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
+    "root": true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.eslint.json',
-    tsconfigRootDir: 'learning_unit_tests_and_static_analysis/learning-tests',
-  },
-  plugins: [
-    '@typescript-eslint',
-    'react',
-  ],
-  rules: {
-  },
-};
+    "extends": [
+        "airbnb",
+        "airbnb-typescript",
+        "airbnb/hooks",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:jsx-a11y/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+
+        "project": './tsconfig.json'
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
+    }
+}
